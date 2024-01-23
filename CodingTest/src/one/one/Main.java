1276511afd7358a98824cb1b4cc081fe6f25198d
hyunchang Park <hyunchang1234@hanmail.net>
2024-01-23 14:23:43 +0900

    1.3 까지 완료

--- CodingTest/src/one/one/Main.java
+++ CodingTest/src/one/one/Main.java
@@ -1,4 +1,28 @@
+/*
+1. 문자 찾기
+
+설명
+한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요.
+대소문자를 구분하지 않습니다.문자열의 길이는 100을 넘지 않습니다.
+
+
+입력
+첫 줄에 문자열이 주어지고, 두 번째 줄에 문자가 주어진다.
+문자열은 영어 알파벳으로만 구성되어 있습니다.
+
+
+출력
+첫 줄에 해당 문자의 개수를 출력한다.
+
+예시 입력 1 
+Computercooler
+c
+
+예시 출력 1
+2
+*/
 package one.one;
+
 
 import java.util.Scanner;
 
@@ -9,9 +33,9 @@ public class Main {
 		t=Character.toUpperCase(t);
 		//System.out.println(str+" "+t);
 		/*for(int i=0; i<str.length(); i++){
-			if(str.charAt(i)==t) answer++;
+			if(str.charAt(i)==t) answer++; // 스트링 인덱스로 접근 = charAt
 		}*/
-		for(char x : str.toCharArray()){
+		for(char x : str.toCharArray()){ // for each 문에선 string 을 문자배열로 만들어서 해야해.
 			if(x==t) answer++;
 		}
 		return answer;
--- CodingTest/src/one/two/Main.java
+++ CodingTest/src/one/two/Main.java
@@ -1,13 +1,64 @@
+/*
+2. 대소문자 변환
+설명
+대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+
+
+입력
+첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
+문자열은 영어 알파벳으로만 구성되어 있습니다.
+
+
+출력
+첫 줄에 대문자는 소문자로, 소문자는 대문자로 변환된 문자열을 출력합니다.
+
+
+예시 입력 1 
+StuDY
+
+예시 출력 1
+sTUdy
+
+*/
 package one.two;
 
-import java.util.Scanner;
+import java.util.*;
+class Main {	
+	public String solution(String str){
+		String answer="";
+		for(char x : str.toCharArray()){
+			if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x); // isLowerCase 소문자냐 하고 물어보는거
+			else answer+=Character.toLowerCase(x);
 
-public class Main {
-  public static void main(String[] args){
-    Scanner in=new Scanner(System.in);
-    int input1 = in.nextInt();
-    int input2 = in.nextInt();
-    System.out.println(input1 + input2);
-    return ;
-  }
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.print(T.solution(str));
+	}
 }
+
+/*
+import java.util.*;
+class Main {	
+	public String solution(String str){
+		String answer="";
+		for(char x : str.toCharArray()){
+			if(x>=97 && x<=122) answer+=(char)(x-32);  // 아스키 넘버 -> 대문자 : A(65)~Z(90) / 소문자 : a(97) ~ z(122) // 32만큼 차이나
+			else answer+=(char)(x+32);
+		}
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.print(T.solution(str));
+	}
+}
+*/
