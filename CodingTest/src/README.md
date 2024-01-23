b03b8cce57ac346449b1cf03bd14cec9088c24bc
hyunchang Park <hyunchang1234@hanmail.net>
2024-01-23 13:30:16 +0900

    README 등록

---
+++ CodingTest/src/README.md
@@ -0,0 +1,20 @@
+# CodingTest
+
+## git command
+```
+
+echo "# CodingTest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ParkHyunchang/CodingTest.git
+git push -u origin main
+
+========================================================================
+
+git add .
+git commit -m "첫 커밋 완료"
+git push -u origin main
+
+```
--- CodingTest/src/one/one/Main.java
+++ CodingTest/src/one/one/Main.java
@@ -3,11 +3,25 @@ package one.one;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args){
-    Scanner in=new Scanner(System.in);
-    int input1 = in.nextInt();
-    int input2 = in.nextInt();
-    System.out.println(input1 + input2);
-    return ;
-  }
+	public int solution(String str, char t){ // non static , instance method
+		int answer=0;
+		str=str.toUpperCase();
+		t=Character.toUpperCase(t);
+		//System.out.println(str+" "+t);
+		/*for(int i=0; i<str.length(); i++){
+			if(str.charAt(i)==t) answer++;
+		}*/
+		for(char x : str.toCharArray()){
+			if(x==t) answer++;
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args){
+		Main T = new Main(); // static 에서 instance method 호출 하려면 객체생성 필요
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next(); // 콘솔로 문자열 하나 읽어 들이는 것
+		char c=kb.next().charAt(0); // charAt 스트링에 인덱스로 접근하는 것
+		System.out.print(T.solution(str, c));
+	}
 }
